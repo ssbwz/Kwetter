a918e793e593eb32487e9c6b5f9350d0f3361693
Saeed Ba Wazir <I488364@fontysict.nl>
2024-06-12 18:12:03 +0200

    Adding advanced setting model

--- frontend/src/components/TweetComponent.jsx
+++ frontend/src/components/TweetComponent.jsx
@@ -6,20 +6,28 @@ import {
     MDBCheckbox,
     MDBIcon,
     MDBBtn,
-    MDBInput
+    MDBInput,
+    MDBModal,
+    MDBModalDialog,
+    MDBModalContent,
+    MDBModalHeader,
+    MDBModalTitle,
+    MDBModalBody,
+    MDBModalFooter,
 }
     from 'mdb-react-ui-kit';
 import { useState } from "react";
 import tweetService from '../services/TweetServer';
 import './style/tweetcomponent.css'
 
-
 function TweetComponent() {
 
     const [tweetText, setTweetText] = useState('');
     const [vTweetText, setVTweetText] = useState("What's happening?");
     const [isEighteenPlus, setIsEighteenPlus] = useState(false);
+    const [basicModal, setBasicModal] = useState(false);
 
+    const toggleOpen = () => setBasicModal(!basicModal);
     async function shareTweet(e) {
         e.preventDefault()
         if (!tweetText) {
@@ -37,32 +45,51 @@ function TweetComponent() {
         setVTweetText("Tweet shared.")
         setTimeout(() => {
             setVTweetText("What's happening?")
-        }, 1000);
+        }, 1500);
     }
-    //<MDBCheckbox name='flexCheck' onClick={e => setIsEighteenPlus(!isEighteenPlus)} checked={isEighteenPlus} id='flexCheckDefault' label='Is this 18+ content?' />
-    
+
+    const options = <>
+        <MDBModal open={basicModal} onClose={() => setBasicModal(false)} tabIndex='-1'>
+            <MDBModalDialog>
+                <MDBModalContent>
+                    <MDBModalHeader>
+                        <MDBModalTitle>Advanced settings</MDBModalTitle>
+                        <MDBBtn className='btn-close' color='none' onClick={toggleOpen}></MDBBtn>
+                    </MDBModalHeader>
+                    <MDBModalBody>
+                        <MDBCheckbox name='flexCheck' onClick={e => setIsEighteenPlus(!isEighteenPlus)} checked={isEighteenPlus} id='flexCheckDefault' label='Is this 18+ content?' />
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                        <MDBBtn color='secondary' onClick={toggleOpen}>
+                            Close
+                        </MDBBtn>
+                    </MDBModalFooter>
+                </MDBModalContent>
+            </MDBModalDialog>
+        </MDBModal>
+    </>
     return <>
         <MDBCard className='box'>
             <MDBCardBody>
                 <MDBRow >
-                    <MDBCol style={{display: "flex", padding: '0 10px 10px 0'}} >
-                    <img
-          src='https://kwetter001.blob.core.windows.net/profile/userPic.jpg'
-          className='img-fluid rounded-circle'
-          alt='User image'
-          id='createTweetprofileImage'
-        />
+                    <MDBCol style={{ display: "flex", padding: '0 10px 10px 0' }} >
+                        <img
+                            src='https://kwetter001.blob.core.windows.net/profile/userPic.jpg'
+                            className='img-fluid rounded-circle'
+                            alt='User image'
+                            id='createTweetprofileImage'
+                        />
 
                         <MDBInput label={vTweetText} id="tweetform" onChange={e => setTweetText(e.target.value)} value={tweetText} rows="{2}" maxLength={140} />
                     </MDBCol>
                 </MDBRow >
                 <MDBRow >
                     <MDBCol size="md toolbar" >
-                    <MDBIcon far icon="image" style={{marginLeft: "3rem"}}  className='icon'/>
-                    <MDBIcon fas icon="poll"  className='icon'/>
-                    <MDBIcon far icon="smile" className='icon'/>
-                    <MDBIcon fas icon="ellipsis-h"  className='icon'/>
-
+                        <MDBIcon far icon="image" style={{ marginLeft: "3rem" }} className='icon' />
+                        <MDBIcon fas icon="poll" className='icon' />
+                        <MDBIcon far icon="smile" className='icon' />
+                        <MDBIcon style={{ cursor: "pointer" }} onClick={toggleOpen} fas icon="ellipsis-h" className='icon' />
+                        {options}
                     </MDBCol>
                     <MDBCol id="col-btn-tweet" size="auto" >
                         <MDBBtn id='btn-tweet' onClick={e => shareTweet(e)} size='lg'>Tweet</MDBBtn>
