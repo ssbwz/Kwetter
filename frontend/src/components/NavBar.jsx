693b09ab6e93c91c0d210d2addd5a0b929b9c45c
Saeed Ba Wazir <I488364@fontysict.nl>
2024-06-19 18:17:43 +0200

    Updating navbar style

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,14 +3,24 @@ import {
     MDBNavbar,
     MDBNavbarBrand,
     MDBIcon,
-    MDBCol
+    MDBCol,
+    MDBNavbarToggler,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBBtn,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+    MDBCollapse,
 } from 'mdb-react-ui-kit';
-
 import { CookiesProvider, useCookies } from 'react-cookie'
 
 import { useNavigate } from 'react-router-dom';
 import IdentitiesServer from '../services/IdentitiesServer';
 import "./style/navbar.css"
+import { useState } from 'react';
 
 function NavBar() {
 
@@ -20,7 +30,8 @@ function NavBar() {
         removeCookie('token')
         navigate("/login")
     }
-
+    const [firstSection, setFirstSection] = useState(false);
+    const [secondSection, setSecondSection] = useState(false);
 
     var links = [
         {
@@ -44,13 +55,6 @@ function NavBar() {
                 icon: "users",
                 path: "/usersmanagement",
                 text: "Users management"
-            },
-            {
-                id: 3,
-                icon: "sign-out-alt",
-                path: "",
-                text: "Log out",
-                onClick: logOut
             }
         ]
     }
@@ -63,48 +67,69 @@ function NavBar() {
                 icon: "home",
                 path: "/",
                 text: "Home"
-            },
-            {
-                id: 2,
-                icon: "user",
-                path: "/me",
-                text: "Profile"
-            },
-            {
-                id: 3,
-                icon: "sign-out-alt",
-                path: "",
-                text: "Log out",
-                onClick: logOut
             }
         ]
     }
 
+    function navigatetor(e, path) {
+        e.preventDefault();
+        navigate(path)
+    }
+
     if (cookies.token) {
         return (
-            <MDBCol className='col-navbar' size='2'>
+            <>
+                <MDBNavbar expand='lg' light bgColor='light'>
+                    <MDBContainer fluid>
+                        <MDBNavbarToggler
+                            aria-controls='navbarSupportedContent'
+                            aria-expanded='false'
+                            aria-label='Toggle navigation'
+                            onClick={() => setFirstSection(!firstSection)}
+                        >
+                            <MDBIcon icon='bars' fas />
+                        </MDBNavbarToggler>
+                        <MDBCollapse navbar open={firstSection}>
+                            <MDBNavbarNav id='startnavbar'>
+                                {links.map(link => {
+                                    return (
+                                        <>
+                                            <MDBNavbarItem>
+                                                <MDBNavbarLink className='link-icon' onClick={e => navigatetor(e, link.path)} key={link.id} href='#'> <MDBIcon fas icon={link.icon} size='xl' className='link-icon' /> {link.text}
+                                                </MDBNavbarLink>
+                                            </MDBNavbarItem>
+                                        </>
+                                    )
+                                })}
+                            </MDBNavbarNav>
+                        </MDBCollapse>
+                        <MDBNavbarBrand id="midNavbar" onClick={e => navigatetor(e, "/")} href='#' > <MDBIcon size='2x' fab icon="twitter" className='icon-twitter' /> </MDBNavbarBrand>
+                        <MDBNavbarNav id='endnavbar' className='justify-content-end'>
+                            <MDBNavbarItem >
+                                <div class="dropdown">
+                                    <img
+                                        data-mdb-button-init
+                                        data-mdb-ripple-init data-mdb-dropdown-init class="btn btn-primary dropdown-toggle"
+                                        type="button"
+                                        id="dropdownMenuButton"
+                                        data-mdb-toggle="dropdown"
+                                        aria-expanded="false"
+                                        src='https://kwetter001.blob.core.windows.net/profile/userPic.jpg'
+                                        className='img-fluid rounded-circle'
+                                        alt='User image'
+                                        onClick={e => navigatetor(e, "/me")}
+                                    />
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <li><a class="dropdown-item" href="#"><MDBIcon fas icon="cog" className='link-icon' />Settings</a></li>
+                                        <li><a class="dropdown-item" onClick={logOut} link><MDBIcon fas icon="sign-out-alt" className='link-icon' /> log out</a></li>
+                                    </ul>
+                                </div>
+                            </MDBNavbarItem>
+                        </MDBNavbarNav>
+                    </MDBContainer>
+                </MDBNavbar>
+            </>
 
-            <CookiesProvider>
-                 <MDBNavbar className='link-navbar'>
-                                <MDBContainer className="link-container-twitter" fluid>
-                                    <MDBNavbarBrand className='link-brand-twitter' href="/"> <MDBIcon size='2x' fab icon="twitter" className='link-icon-twitter' /> </MDBNavbarBrand>
-                                    <br />
-                                </MDBContainer>
-                            </MDBNavbar>
-                {links.map(link => {
-                    return (
-                        <>
-                            <MDBNavbar className='link-navbar'>
-                                <MDBContainer className="link-container" fluid>
-                                    <MDBNavbarBrand onClick={link.onClick} key={link.id} href={link.path}> <MDBIcon fas icon={link.icon} className='link-icon' /> {link.text}</MDBNavbarBrand>
-                                    <br />
-                                </MDBContainer>
-                            </MDBNavbar>
-                        </>
-                    )
-                })}
-            </CookiesProvider>
-                        </MDBCol>
         );
     }
     else {
